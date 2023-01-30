449a4fb21bca2d8296af85d2a69af5df5e99181a
tharikf <tharikff1@gmail.com>
2023-01-30 20:07:10 -0300

    Alteracoes no projeto

---
+++ Modulos/FUNCAO_Retomando_Valores_Previsao.py
@@ -0,0 +1,26 @@
+
+'''
+Essa função realiza a transformação de volta de dados previstos no dataset diferenciado para a escala original.
+'''
+
+
+import pandas as pd
+
+def retomando_valor_previsto(dataframe_original, dataframe_diferenciado):
+    
+    # Lista Vazia
+    lista_vazia = []
+    
+    # Voltando valores para escala normal (sem diferenciação)
+    for data, valor_original, previsao_diferenciada in zip(dataframe_diferenciado.index, dataframe_original.iloc[1:, ], dataframe_diferenciado):
+        valor_previsto = round((valor_original + previsao_diferenciada), 2)
+        lista_vazia.append([data, valor_original, valor_previsto])
+    
+    df_resultados = pd.DataFrame(lista_vazia)
+    df_resultados.columns = ['Data', 'Preco_Original', 'Preco_Previsto']
+    df_resultados.set_index('Data', inplace = True)
+    return df_resultados
+
+
+
+
