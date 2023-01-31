a6786371ae825d51f072e7d9974eb2e5cc7b4b7a
tharikf <tharikff1@gmail.com>
2023-01-31 19:45:13 -0300

    Alteracoes no Projeto

--- Modulos/FUNCAO_Media_Movel.py
+++ Modulos/FUNCAO_Media_Movel.py
@@ -11,17 +11,18 @@ from sklearn.metrics import mean_absolute_error
 def calculo_media_movel(dataframe, coluna, dias):
     
     # Media movel
-    dataframe['media_movel'] = round(dataframe[coluna].rolling(window = dias).mean(), 2)
+    df_copia = dataframe.copy()
+    df_copia['media_movel'] = round(df_copia[coluna].rolling(window = dias).mean(), 2)
     
     # Retirando valores NA
-    dataframe = dataframe.dropna()
+    df_copia = df_copia.dropna()
     
     # Criando intervalo de confiança (95% de confiança - 1.96)
-    erro_medio = mean_absolute_error(dataframe['preco_reais'], dataframe['media_movel'])
-    desvio_padrao = np.std(dataframe['preco_reais'] - dataframe['media_movel'])
-    dataframe['mm_limite_abaixo'] = round(dataframe['media_movel'] - (erro_medio + (1.96 * desvio_padrao)), 2)
-    dataframe['mm_limite_acima'] = round(dataframe['media_movel'] + (erro_medio + (1.96 * desvio_padrao)), 2)
+    erro_medio = mean_absolute_error(df_copia['preco_reais'], df_copia['media_movel'])
+    desvio_padrao = np.std(df_copia['preco_reais'] - df_copia['media_movel'])
+    df_copia['mm_limite_abaixo'] = round(df_copia['media_movel'] - (erro_medio + (1.96 * desvio_padrao)), 2)
+    df_copia['mm_limite_acima'] = round(df_copia['media_movel'] + (erro_medio + (1.96 * desvio_padrao)), 2)
     
     
-    return dataframe
+    return df_copia
 
--- Modulos/FUNCAO_Retomando_Valores_Previsao.py
+++ Modulos/FUNCAO_Retomando_Valores_Previsao.py
@@ -12,9 +12,9 @@ def retomando_valor_previsto(dataframe_original, dataframe_diferenciado):
     lista_vazia = []
     
     # Voltando valores para escala normal (sem diferenciação)
-    for data, valor_original, previsao_diferenciada in zip(dataframe_diferenciado.index, dataframe_original.iloc[1:, ], dataframe_diferenciado):
+    for data, valor_original_df, valor_original, previsao_diferenciada in zip(dataframe_original.iloc[1:,].index, dataframe_original.iloc[1:,], dataframe_original, dataframe_diferenciado):
         valor_previsto = round((valor_original + previsao_diferenciada), 2)
-        lista_vazia.append([data, valor_original, valor_previsto])
+        lista_vazia.append([data, valor_original_df, valor_previsto])
     
     df_resultados = pd.DataFrame(lista_vazia)
     df_resultados.columns = ['Data', 'Preco_Original', 'Preco_Previsto']
